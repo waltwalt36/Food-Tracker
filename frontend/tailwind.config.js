83ba60d43c5c510bd130d290ea63444812a78068
Walter <wbrione@clemson.edu>
2026-01-10 20:18:19 -0500

    Fixed the tailwind stuff finally after 10 years of trouble shooting

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // <-- look inside src/ for all JSX/JS files
+    "./src/**/*.{js,jsx,ts,tsx,html}", // <-- look inside src/ for all JSX/JS files
     // "./public/index.html",      // uncomment if you later have a public/index.html
   ],
   theme: {
